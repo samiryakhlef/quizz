323095fa629648e8d1580800b429493d925abc4a
samiryakhlef <samir.yakhlef@gmail.com>
2021-12-29 16:35:12 +0100

    maj

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-start();
+start()
 // Il faut la définir en “async” (asynchrone)
 // car elle utilise fetch() (et “await” pour attendre une réponse)
 async function start() {
@@ -11,7 +11,15 @@ async function start() {
 
   // Affiche les question dans la div id "question1","question2","question3","question4"
   document.getElementById("question1").innerHTML = (data[0].question);
+  document.getElementById("label_response1").innerHTML = (data[0].propositions[0]);
+  document.getElementById("label_response2").innerHTML = (data[0].propositions[1]);
+  document.getElementById("label_response3").innerHTML = (data[0].propositions[2]);
+  document.getElementById("label_response4").innerHTML = (data[0].propositions[3]);
   document.getElementById("question2").innerHTML = (data[1].question);
+  document.getElementById("label_response1").innerHTML = (data[1].propositions[0]);
+  document.getElementById("label_response2").innerHTML = (data[1].propositions[1]);
+  document.getElementById("label_response3").innerHTML = (data[1].propositions[2]);
+  document.getElementById("label_response4").innerHTML = (data[1].propositions[3]);
   document.getElementById("question3").innerHTML = (data[2].question);
   document.getElementById("question4").innerHTML = (data[3].question);
 }
@@ -31,10 +39,11 @@ function shuffleArray(array) {
   return array.sort(() => 0.5 - Math.random());
 }
 
-//je créer une function pour récupèrer les propositions
-function  propositions(){
-
+//Créer une function pour récupèrer les propositions et les afficher dans les label_response
+function  reponse(){
+const json = data.json(); 
+const objet = JSON.parse(json).proposistion;
   
-//j'affiche les propositions dans le label_response
-document.getElementsByClassName("label_response").innerHTML = (data[[0],[1],[2],[3].propositions]);
+//Afficher les propositions dans le label class="label_response"
+
 }
