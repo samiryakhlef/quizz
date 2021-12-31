970dbd0b0e29fc5944d1500696cdccac20713c2e
vincent sureau <vincentsureau5@gmail.com>
2021-12-31 11:26:01 +0100

    add pseudo code for logic

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ async function start() {
   document.getElementById("label_response4").innerHTML =
     data[0].propositions[3];
   document.getElementById("question2").innerHTML = data[1].question;
-  document.getElementById("label_reponse1").innerHTML = data[1].propositions[0];
+  // exemple pour refactoriser
+  // for(var i = 1; i <= 4; i++) {
+  //   document.getElementById("label_response" + i).innerHTML = data[1].propositions[i];
+  // }
+  document.getElementById("label_reponse1").innerHTML = data[1].id;
   document.getElementById("label_reponse2").innerHTML = data[1].propositions[1];
   document.getElementById("label_reponse3").innerHTML = data[1].propositions[2];
   document.getElementById("label_reponse4").innerHTML = data[1].propositions[3];
@@ -32,6 +36,10 @@ async function start() {
   document.getElementById("label_ponse1").innerHTML = data[3].propositions[0];
   document.getElementById("label_ponse3").innerHTML = data[3].propositions[2];
   document.getElementById("label_ponse4").innerHTML = data[3].propositions[3];
+
+  // j'ajoute à chaque input value : data[].proposition[]
+  // j'ajoute à chaque input la name : data[].id
+  //exemple : document.getElementById("input_reponse1").name = data[1].propositions[0];
 }
 
 // Fonction de RÉCUPÉRATION DU FICHIER JSON contenant le quiz
@@ -39,7 +47,7 @@ async function start() {
 async function getJson() {
   // Récupère les données avec la fonction fetch()
   // Ici le fichier s'appelle quiz.json et il est situé à la racine "/" du dossier projet
-  const data = await fetch("http://localhost:5500/quiz.json");
+  const data = await fetch("http://127.0.0.1/simplon/quizz/samir/quiz.json");
 
   return data.json(); // Retourne les données au format Json
 }
@@ -52,11 +60,24 @@ function shuffleArray(array) {
 // évènement au click du bouton valider
 
 document.querySelector('form').addEventListener('submit', function (e) {
-  var label = document.getElementByTagName('label')
-  if (label.value === true) {
-    document.getElementById('good').add('green')
-  }
-})
+  //e.preventDefault();
+  // je définie une variable score = 0
+
+  // je récupère toutes les réponses du formulaire (input selectionnés par l'utilisateur)
+
+  // pour chaque input, je récupère la valeur de l'input (input.value)
+
+  // pour chaque input, je récupềre l'id de la question (input.name)
+
+  // pour chaque input, je récupère la bonne réponse dans le json (le name de l'input = id de la question dans le json)
+
+  // si la valeur de l'input est égale à la bonne réponse, je rajoute la classe "good" à l'input
+  // et j'incrémente le score
+
+  // si la valeur de l'input est différente de la bonne réponse, je rajoute la classe "bad" à l'input
+
+  // j'affiche le score dans la div id "score"
+
 
 /*for (var i = 0; i < boutons.length; i++){
     var bouton = boutons[i];
@@ -67,3 +88,4 @@ document.querySelector('form').addEventListener('submit', function (e) {
       return document.getElementsByClassName('bad').add('red').click(e);
     })
 }*/
+});
