3974f5f44dac674a6ae8a026a9cc14bebb470f95
samiryakhlef <samir.yakhlef@gmail.com>
2022-01-18 15:04:11 +0100

    maj

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 start();
+
 // Il faut la définir en “async” (asynchrone)
 // car elle utilise fetch() (et “await” pour attendre une réponse)
 async function start() {
-  const data = await getJson(); // Récupère le tableau json
+  var data = await getJson(); // Récupère le tableau json
 
   // Mélange le tableau json
   shuffleArray(data);
+
+  // Coupe le tableau pour ne garder que les 4 premières questions
+  data = data.slice(0, 4);
 
   // Fonction RÉCUPÉRATION DU FICHIER JSON contenant le quiz
   // en “async” car elle utilise fetch() et “await”
@@ -15,11 +19,6 @@ async function start() {
     const data = await fetch("http://localhost:5500/quiz.json");
 
     return data.json(); // Retourne les données au format Json
-  }
-
-  // Fonction MÉLANGE ALÉATOIRE du tableau
-  function shuffleArray(array) {
-    return array.sort(() => 0.5 - Math.random());
   }
 
   // Affiche les question et les propositions
@@ -51,36 +50,56 @@ async function start() {
   document.getElementById("label_ponse3").innerHTML = data[3].propositions[2];
   document.getElementById("label_ponse4").innerHTML = data[3].propositions[3];
 
+  document.querySelector("form").addEventListener("submit", validation);
 
-  document.querySelector('form').addEventListener('submit',e=> {
-    e.preventDefault();
-    
-    
+  function validation(event) {
+    event.preventDefault();
+
     // je définie une variable score = 0
     var score = 0;
-    // je récupère toutes les réponses du formulaire (input selectionnés par l'utilisateur)
-    var formData = document.querySelector('form').value; 
-    // pour chaque input, je récupère la valeur de l'input (input.value)
-    var inputData = document.querySelector('input').value;
-    // pour chaque input, je récupềre l'id de la question (input.name)
-    inputData = parseInt(document.querySelector('#input').value);
-    // pour chaque input, je récupère la bonne réponse dans le json (le name de l'input = id de la question dans le json)
-    inputData = data[id].reponse;
-    console.log(data[id].reponse);
-    // si la valeur de l'input est égale à la bonne réponse, je rajoute la classe "good" à l'input
-    for (i = 0; i <= inputData.length ; i++) {
-    if (formData && inputData === document.querySelector('input').data[0]) {
-    
+
+    // Compte le score en parcourant les 4 questions
+    for (let qIndex = 0; qIndex < data.length; qIndex++) {
+      // Récupére les propositions json
+      var propositions = data[qIndex].propositions;
+      // SelectorAll des 4 <input> de la question ('#question0 input')
+      var input = document.querySelectorAll("#question" + qIndex + " input");
+
+      for (let propIndex = 0; propIndex < propositions.length; propIndex++) {
+        // Définit la value de l'<input>
+        input[propIndex].setAttribute("value", propositions[propIndex]);
+      }
+      // je récupère toutes les réponses de cette question
+      // (input selectionné par l'utilisateur)
+      inputChecked = document.querySelector(
+        "input[name = question" + qIndex + "]:checked"
+      );
+
+      // Si la réponse n'est pas vide
+      if (inputChecked) {
+        var anwser = inputChecked.value; // Réponse choisie
+
+        // pour chaque input, je récupère la bonne réponse dans le json (le name de l'input = id de la question dans le json)
+        var goodAnswer = data[qIndex].reponse; // Bonne réponse
+
+        // Si la réponse est bonne
+        if (anwser == goodAnswer) {
+          score++; // +1 point
+        }
+      }
     }
-}
-    }
-  )
-    // et j'incrémente le score
-    
-    //if ()
-    // si la valeur de l'input est différente de la bonne réponse, je rajoute la classe "bad" à l'input
-    // j'affiche le score dans la div id "score"
-    // VALIDATION DU QUIZ*/
+    document.getElementById("score").innerHTML = score;
+  }
+
+  // et j'incrémente le score
+
+  //if ()
+  // si la valeur de l'input est différente de la bonne réponse, je rajoute la classe "bad" à l'input
+  // j'affiche le score dans la div id "score"
+  // VALIDATION DU QUIZ*/
 }
 
-
+// Fonction MÉLANGE ALÉATOIRE du tableau
+function shuffleArray(array) {
+  return array.sort(() => 0.5 - Math.random());
+}
